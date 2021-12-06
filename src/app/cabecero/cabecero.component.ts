a8823ad78b376e774181c23416707deb7443d853
melrouse <melisamr@hotmail.com>
2021-12-06 15:38:06 -0300

    proyecto presupuesto

---
+++ src/app/cabecero/cabecero.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecero',
+  templateUrl: './cabecero.component.html',
+  styleUrls: ['./cabecero.component.css'],
+})
+export class CabeceroComponent implements OnInit {
+  @Input() presupuestoTotal: number = 0;
+  @Input() ingresoTotal: number = 0;
+  @Input() egresoTotal: number = 0;
+  @Input() procentajeTotal: number = 0;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
